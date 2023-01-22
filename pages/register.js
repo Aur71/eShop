72b60c41a9dab272ad69605ce46d192f6d9fb9af
Aur71 <111057774+Aur71@users.noreply.github.com>
2023-01-22 11:04:47 +0200

    Register page done

--- pages/register.js
+++ pages/register.js
@@ -1,7 +1,67 @@
 import styles from '../styles/account/Register.module.scss';
 
+// TOOLS
+import Image from 'next/image';
+import Link from 'next/link';
+
+// MEDIA
+import { BsGoogle } from 'react-icons/bs';
+import logo from '../public/logo.png';
+
 const register = () => {
-  return <div>register</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.register}>
+        <div className={styles.logoContainer}>
+          <Link href='/'>
+            <Image src={logo} alt='GMAG logo' />
+          </Link>
+        </div>
+
+        <form>
+          <h1>Register</h1>
+
+          <label htmlFor='email'>Email:</label>
+          <input
+            type='email'
+            id='email'
+            name='email'
+            placeholder='user@gmail.com'
+          />
+
+          <label htmlFor='email'>Password:</label>
+          <input
+            type='password'
+            id='password'
+            name='password'
+            placeholder='abcDEF123!'
+          />
+
+          <button type='submit' className={styles.submitBtn}>
+            Next
+          </button>
+
+          <div className={styles.line}>
+            <span></span>
+            <span>or</span>
+            <span></span>
+          </div>
+
+          <button className={styles.googleBtn}>
+            <span>
+              <BsGoogle />
+            </span>
+
+            <span>Google</span>
+          </button>
+        </form>
+
+        <Link href='/login' className={styles.registerLink}>
+          Login
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default register;
