0435fe0fb987ebd2e7eff2dfb2ab990b46cb7527
Aur71 <111057774+Aur71@users.noreply.github.com>
2023-01-23 15:31:34 +0200

    account data section added

--- components/account/MyAccount.jsx
+++ components/account/MyAccount.jsx
@@ -4,10 +4,49 @@ import styles from '../../styles/account/components/MyAccount.module.scss';
 import Image from 'next/image';
 
 // MEDIA
-import user from '../../public/user.webp';
+import user from '../../public/user.png';
 
 const MyAccount = () => {
-  return <div className={styles.myAccount}></div>;
+  return (
+    <div className={styles.myAccount}>
+      <div className={styles.accountData}>
+        <h2>Account Data</h2>
+
+        <div className={styles.data}>
+          <div className={styles.imgContainer}>
+            <input type='file' accept='image/png, image/jpeg' />
+
+            <Image src={user} alt='default user image' />
+          </div>
+
+          <div className={styles.dataContainer}>
+            <ul>
+              <li>
+                <span>Name:</span>
+                <span>Aurel</span>
+              </li>
+
+              <li>
+                <span>Email:</span>
+                <span>aurel@gmail.com</span>
+              </li>
+
+              <li>
+                <span>Phone:</span>
+                <span>0771088481</span>
+              </li>
+            </ul>
+          </div>
+
+          <h3>
+            Thank you for being <br /> our customer <br /> 2 days
+          </h3>
+        </div>
+
+        <button className={styles.changeData}>change</button>
+      </div>
+    </div>
+  );
 };
 
 export default MyAccount;
