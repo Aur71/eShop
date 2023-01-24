9f476a8a7a638f6f4c5e63dfa81acb963dd5b761
Aur71 <111057774+Aur71@users.noreply.github.com>
2023-01-24 19:26:38 +0200

    Order details done

--- components/account/orders/OrderDetails.jsx
+++ components/account/orders/OrderDetails.jsx
@@ -1,16 +1,52 @@
 import styles from '../../../styles/account/orders/OrderDetails.module.scss';
+// HOOKS
+import { useState, useEffect } from 'react';
+
+// TOOLS
+import Image from 'next/image';
+import Link from 'next/link';
+
+// COMPONENTS
+import Product from './Product';
 
 // MEDIA
-import { AiOutlineClose } from 'react-icons/ai';
+import { AiOutlineClose, AiOutlineDoubleRight } from 'react-icons/ai';
 
 const OrderDetails = ({ closeDetails }) => {
   return (
     <section className={styles.orderDetails}>
-      <button className={styles.closeBtn} onClick={closeDetails}>
-        <AiOutlineClose />
-      </button>
+      <div className={styles.header}>
+        <div className={styles.path}>
+          <button onClick={closeDetails}>Orders</button>
+          <AiOutlineDoubleRight className={styles.icon} />
+          <p>Order Details</p>
+        </div>
 
-      <div className={styles.header}></div>
+        <button className={styles.closeBtn} onClick={closeDetails}>
+          <AiOutlineClose />
+        </button>
+      </div>
+
+      <h3>Order no. 292427516</h3>
+
+      <div className={styles.info}>
+        <p>
+          <span>Ordered on:</span>
+          <span>19 decembrie 2022, 11:41</span>
+        </p>
+
+        <p>
+          <span>Total:</span>
+          <span>31,39$</span>
+        </p>
+
+        <div className={styles.products}>
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+        </div>
+      </div>
     </section>
   );
 };
